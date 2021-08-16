a6a13ec19fbe8decd45d48ea882b571e208bb246
Mrityunjoy Barman <mrityunjoy527@gmail.com>
2021-08-17 00:11:40 +0530

    Some change

--- Diff.java
+++ Diff.java
@@ -3,6 +3,6 @@ public class Diff {
     public static void main(String[] args){
         int a = 6;
         int v = 5;
-        System.out.println(a - v);
+        System.out.println("The output is " + a - v);
     }
 }
